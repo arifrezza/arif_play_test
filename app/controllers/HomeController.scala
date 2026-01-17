2e84ccaa6d7bd11ef524b13dafcae43af8d2286e
arif <arifrezza@gmail.com>
2026-01-17 22:54:41 +0530

    Form_Practice

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,41 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.mvc._
+import play.api.data.Forms._
+import models.UserData
+import play.api.data.Form
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  //Define a form mapping for UserData
+  val userForm:Form[UserData] = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "age" -> number(min = 0, max = 120)
+    )(UserData.apply)(UserData.unapply)
+  )
+
+  // Action to render the index page
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index(userForm))
+  }
+
+  // Action to handle form submission
+  def processForm: Action[AnyContent] = Action{
+    implicit request: Request[AnyContent] =>
+      userForm.bindFromRequest.fold(
+        formWithErrors => {
+          // If there are errors in the form, re-render the page with errors
+          BadRequest(views.html.index(formWithErrors))
+        },
+        UserData => {
+          // If the form is valid, process the data (e.g., save to database)
+          // For demonstration, we just render a success page
+          Ok(views.html.result(UserData))
+        }
+      )
   }
 }
